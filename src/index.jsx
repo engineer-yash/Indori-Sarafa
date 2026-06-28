5e1491844c77771875aaf5530d0d34ea10e3e785
Yash Gohel <gohelyash94@gmail.com>
2026-06-28 21:23:37 +0530

    okkkk

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
-import App from "@/App";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
