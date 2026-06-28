ee65007525300fa4f089ee554ea271451303a94e
Yash Gohel <gohelyash94@gmail.com>
2026-06-28 21:22:18 +0530

    no prob

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
-import "@/index.css";
+import "./index.css";
 import App from "@/App";
 
 const queryClient = new QueryClient({
